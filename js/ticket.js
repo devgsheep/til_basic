97604291b814c0852fc1103d7a527a65562befa3
devgsheep <dev.gsheep@gmail.com>
2025-06-16 18:12:13 +0900

    [docs] trip api연동

--- js/ticket.js
+++ js/ticket.js
@@ -1,6 +1,52 @@
 window.addEventListener("load", function () {
   // swiper 만들기 실행
 
+  //json 데이터
+  const ticketApiData = [
+    {
+      링크: "#",
+      이미지:
+        "https://ticketimage.interpark.com/Play/image/large/25/25008376_p.gif",
+      alt: "슬립노모어 포스터",
+      순위: "1",
+      제목: "〈슬립노모어 서울〉(Sleep No More Seoul)",
+      장소: "매키탄 호텔 (The McKithan Hotel)",
+      날짜: "2025.07.24 - 2025.08.09",
+      옵션: ["단독판매", "좌석우위"],
+    },
+  ];
+
+  //어디에 html 배치할것인지
+  const ticketPos = document.querySelector(".sw_ticket .swiper-wrapper");
+  let html = `
+                  <div class="swiper-slide">
+                    <a href=${ticketApiData[0].링크} class="ticket_slide_item">
+                      <div class="ticket_image">
+                        <img
+                          src=${ticketApiData[0].이미지}
+                          alt=${ticketApiData[0].alt}
+                        />
+                        <div class="ticket_rank">${ticketApiData[0].순위}</div>
+                      </div>
+                      <div class="ticket_item_info">
+                        <p class="ticket_item_name">
+                          ${ticketApiData[0].제목}
+                        </p>
+                        <p class="ticket_item_place">
+                          ${ticketApiData[0].장소}
+                        </p>
+                        <p class="ticket_item_date">${ticketApiData[0].날짜}</p>
+                        <div class="ticket_item_option">
+                          <span class="ticket_item_red">${ticketApiData[0].옵션[0]}</span>
+                          <span class="ticket_item_blue">${ticketApiData[0].옵션[1]}</span>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+`;
+
+  ticketPos.innerHTML = html;
+
   //포커스 되었을 때 적용될 포커스 이름
   const focusName = "ticket_focus";
   const bts = document.querySelectorAll(".ticket_button_list li button");
